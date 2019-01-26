709f1f4cae6cb7c1ee66789ca43dc65e1ea00b59
Pascal Vautour <vautour.pascal@gmail.com>
2019-01-26 13:18:43 -0500

    inbox works

--- interface/courriel.js
+++ interface/courriel.js
@@ -1,3 +1,8 @@
+var inboxVariables = {
+  mail : [mail('max@gmail.com','max has answered your question on kijiji.'), mail('spam@gmail.com','Make 100 000$ a day with this simple trick'), mail('uqo@gmail.com','Make 100 000$ a day with this simple trick')],
+  displayedMail : mail
+}
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -20,3 +25,28 @@ function openNav() {
     })
     document.getElementById(target).style.display = "block";
   } 
+
+  function searchMail(value) {
+    console.log(inboxVariables.mail[0].sender)
+    inboxVariables.displayedMail = [];
+    var result = "<caption>Courrier</caption><tr><th>Sent by</th><th>Content</th></tr>";
+    inboxVariables.mail.forEach(element => {
+      if (element.sender.includes(value) || element.content.includes(value)) {
+        inboxVariables.displayedMail.push(element);
+      }
+    })
+  
+    inboxVariables.displayedMail.forEach( element => {
+      result += ('<tr><td>' + element.sender + '</td><td>' + element.content + '</td></tr>');
+    })
+
+    if(!inboxVariables.displayedMail) {
+      result += "<tr text-alight: 'center'>no results...</tr>";
+    }
+
+    document.getElementById('displayedMail').innerHTML = result;
+  }
+  
+  function mail(sender, content) {
+    return ({sender: sender, content: content});
+  }
