a07c23531e38c21de5fd688ac3e8c6ddb6f56afa
Pascal Vautour <vautour.pascal@gmail.com>
2019-03-30 17:44:23 -0400

    adjust regex

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,11 +10,13 @@ router.get('/', function(req, res, next) {
 
 /* GET mail. */
 router.get('/getLetters', function(req, res, next) {
-  var pemRegex = new RegExp("-----BEGIN PUBLIC KEY-----(.|\n|\r)*-----END PUBLIC KEY-----\r?\n?$");
-
-  ret = ["test", "patate"];
+  let pemRegex = new RegExp("^-----BEGIN PUBLIC KEY-----(.|\n|\r)*-----END PUBLIC KEY-----\r?\n?$");
+  let ret = [];
 
   if (pemRegex.test(req.query.pem)) {
+    let RawLetters = fs.readFileSync(path.resolve(__dirname, '../public/database/messages.json'));  
+    let letters = JSON.parse(RawLetters); 
+    ret = letters.messages.map(a => a.dest == req.query.pem);
     res.json(ret);
   } else {
     res.status(400).send('Invalid pem!');
