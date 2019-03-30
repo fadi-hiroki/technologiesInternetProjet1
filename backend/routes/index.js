799b3d1163d8d6d05ccc157e01058cd5d9ad2b88
Pascal Vautour <vautour.pascal@gmail.com>
2019-03-30 14:39:21 -0400

    WIP: getLetters

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET mail. */
+router.get('/getLetters', function(req, res, next) {
+  if(req.params.pem) {
+    console.log("il avait des params")
+    return JSON.stringify(["test", "ceci"]);
+  }
+  next();
+});
+
 module.exports = router;
