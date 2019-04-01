7c04c5064bd994eecf7e39a19e23cfdcae42bcfc
Pascal Vautour <vautour.pascal@gmail.com>
2019-04-01 14:21:12 -0400

    database api added

--- backend/public/javascripts/validations.js
+++ backend/public/javascripts/validations.js
@@ -1,6 +1,10 @@
 module.exports = {
     validPEM(pem) {
         let pemRegex = new RegExp("^-----BEGIN PUBLIC KEY-----(.|\n|\r)*-----END PUBLIC KEY-----\r?\n?$");
-        return pemRegex.test(pem);
+        if(pemRegex.test(pem))
+            return true;
+        else {
+            return false;
+        }
     }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-const path = require("path");
 
 const validations = require("../public/javascripts/validations.js");
+const database = require("../public/javascripts/database.js");
 
 
 /* GET home page. */
@@ -13,40 +12,34 @@ router.get('/', function(req, res, next) {
 
 /* GET mail. */
 router.get('/getLetters', function(req, res, next) {
-  // let pemRegex = new RegExp("^-----BEGIN PUBLIC KEY-----(.|\n|\r)*-----END PUBLIC KEY-----\r?\n?$");
   let ret = [];
 
-  if (validations.validPEM(req.query.pem)) {
-    // if (pemRegex.test(req.query.pem)) {
-    let RawLetters = fs.readFileSync(path.resolve(__dirname, '../public/database/messages.json'));  
-    let letters = JSON.parse(RawLetters); 
+  if (!validations.validPEM(req.query.pem)){
+    res.status(400).send('Invalid pem!');
+  } else {
+    let letters = database.getLetters();
     letters.messages.forEach(function (letter) {
       if(letter.dest == req.query.pem) {
         ret.push(letter);
       }
     })
     res.json(ret);
-  } else {
-    res.status(400).send('Invalid pem!');
   }
 });
 
 /* POST mail. */
 router.post('/addLetters', function(req, res, next) {
-  let pemRegex = new RegExp("^-----BEGIN PUBLIC KEY-----(.|\n|\r)*-----END PUBLIC KEY-----\r?\n?$");
-  let ret = [];
+  let letter;
 
-  if (pemRegex.test(req.query.pem)) {
-    let RawLetters = fs.readFileSync(path.resolve(__dirname, '../public/database/messages.json'));  
-    let letters = JSON.parse(RawLetters); 
-    letters.messages.forEach(function (letter) {
-      if(letter.dest == req.query.pem) {
-        ret.push(letter);
-      }
-    })
-    res.json(ret);
-  } else {
+  if (!validations.validPEM(req.query.dest.pem)){
     res.status(400).send('Invalid pem!');
+  } else {
+    let letters = database.getLetters();
+    letter = {
+      dest: req.query.dest.pem,
+      msg: req.query.dest.msg
+    }
+    letters.push(letter);
   }
 });
 
